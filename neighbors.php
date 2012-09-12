0a3257bb35be0ca923280c1f5ae3174e7827687f
segfault <s9gf4ult@gmail.com>
2012-09-12 11:27:15 +0600

    modified:   neighbors.php 	* recursvie Renderer of categories

--- neighbors.php
+++ neighbors.php
@@ -39,6 +39,8 @@ class neighbors {
 	var $attr;
     
     /** get neighbour name and distance to in meters.
+     * \param $nm name of neighbour as string
+     * \param $d distance to neighbour in meters
      * \return string to insert in mediawiki
      */
     function formatNeighbour($nm, $d) {
@@ -52,6 +54,40 @@ class neighbors {
         $kmch = 5 * 1000 / 60; # 5km/h -> m/min
         $atime = round($d / $kmch) . " минут";                    
         return "* [[$nm]] $dist, примерно $atime пешком";
+    }
+    
+    /** render array to category tree
+     * \param $cattree Array() with category name as key and category children as value, or string. 
+     * If string then consider as titile - category element.
+     * \param $distances Array() with title as key and distnace in meters as value.
+     * \param $prepostfix string appended and prepened to category name when rendering
+     * \return rendered string to insert into page as mediawiki text 
+    */   
+    function renderCategory($cattree, $distances, $prepostfix = "===") {
+        if (is_string($cattree)) {
+            return $this->formatNeighbour($cattree, $distances[$cattree]);
+        } elseif (is_array($cattree)) {
+            $out = array();
+            foreach($cattree as $catname => $childs) {
+                $chstrings = asort(array_filter($childs, function($a) {return is_string($a)}), SORT_STRING);
+                $charrays = asort(array_filter($childs, function($a) {return is_array($a)}), SORT_STRING);
+                $ch1 = array();
+                foreach($chstrings as $chstr) {
+                    array_push($ch1, $this->renderCategory($chstr, $distances, $prepostfix . "="));
+                }
+                $ch2 = array();
+                foreach($charrays as $charr) {
+                    array_push($ch2, $this->renderCategory($charr, $distances, $prepostfix . "="));
+                }
+                array_push($out, "$prepostfix $catname $prepostfix");
+                array_push($out, implode("\n\n", $ch1));
+                array_push($out, implode("\n\n", $ch2)); 
+            }
+            return implode("\n\n", $out);
+        } else {
+            error_log("can not renderCategory, first argument must be string or array");
+            return "";
+        }
     }
 
 	function __construct( $dist )
@@ -170,16 +206,6 @@ class neighbors {
             asort($categories[$cat]);
         }
 
-        # remove ignoring categories        
-        $ignorecat = $wgRequest->getVal("ignorecat");
-        if (!empty($ignorecat)) {
-            $icats = explode(";", $ignorecat);
-            foreach($icats as $ign) {
-                $ic = trim($ign);
-                unset($categories[$ic]);                 
-            }
-        }
-        
         # sort by categories 
         ksort($categories);
         
